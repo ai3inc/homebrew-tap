b29c32edd695d8a8d6ff68e46a2f9619ac20ce04
latemonk <63733683+latemonk@users.noreply.github.com>
2026-09-14 01:07:14 +0000

    chore: update formula to 0.3.227

--- Formula/bizcoder.rb
+++ Formula/bizcoder.rb
@@ -3,28 +3,28 @@
 class Bizcoder < Formula
   desc "Business AI coding agent"
   homepage "https://bizcoder.ai"
-  version "0.3.226"
+  version "0.3.227"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://registry.npmjs.org/bizcoder-darwin-arm64/-/bizcoder-darwin-arm64-0.3.226.tgz"
-      sha256 "3bf86b04017825dc02c286e291d82e0b93096c173f1a75fccc21d317b68f6252"
+      url "https://registry.npmjs.org/bizcoder-darwin-arm64/-/bizcoder-darwin-arm64-0.3.227.tgz"
+      sha256 "9ac6ad90fc259d5ded5c967dde7f7197dde38664c3f2a258894479067a8ca649"
     end
     on_intel do
-      url "https://registry.npmjs.org/bizcoder-darwin-x64/-/bizcoder-darwin-x64-0.3.226.tgz"
-      sha256 "d1e7a0230df7ed1db735217dfd892eead8b23f015a5f01a16244ef8e3757f1c4"
+      url "https://registry.npmjs.org/bizcoder-darwin-x64/-/bizcoder-darwin-x64-0.3.227.tgz"
+      sha256 "c949c8afba8421b2f1286fd3a60d6ae13ef31d98ffaa465fc8cce59ed42d03eb"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://registry.npmjs.org/bizcoder-linux-arm64/-/bizcoder-linux-arm64-0.3.226.tgz"
-      sha256 "f438f71ed8b50368e645c9dd9828de93806a5324406b176bfec4b0933795411d"
+      url "https://registry.npmjs.org/bizcoder-linux-arm64/-/bizcoder-linux-arm64-0.3.227.tgz"
+      sha256 "c48ec36506341e65ee8889de3e824a89fb73f502dcc6b8f155ce771b56bfa474"
     end
     on_intel do
-      url "https://registry.npmjs.org/bizcoder-linux-x64/-/bizcoder-linux-x64-0.3.226.tgz"
-      sha256 "9e21fbc6590667f1b55b83d2e0acbafba9dab8584f610d2f8b728adc5e18023e"
+      url "https://registry.npmjs.org/bizcoder-linux-x64/-/bizcoder-linux-x64-0.3.227.tgz"
+      sha256 "b24f32f3a8ce2b3774e6445fff4b88fdcc96ba0c036b353d51888440cd074b97"
     end
   end
 
